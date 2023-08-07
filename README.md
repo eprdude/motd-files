11021f7c2f0401d8204f27137de840b6b6b3b55d
eprdude <eprdude@protonmail.com>
2023-08-07 17:28:49 -0500

    added classic ubuntu-tux motd file

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # motd-files
 stash for custom motd files
+
+currently using these files in /etc/update-motd.d
+
+or
+
+~/.oh-my-bash/custom
+
+
---
+++ eprdude-ubuntu-logo-classic-tux-motd.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+# Print the Welcome, Ubuntu Logo and System Information
+
+[ -r /etc/lsb-release ] && . /etc/lsb-release
+
+if [ -z "$DISTRIB_DESCRIPTION" ] && [ -x /usr/bin/lsb_release ]; then
+	# Fall back to using the very slow lsb_release utility
+	DISTRIB_DESCRIPTION=$(lsb_release -s -d)
+fi
+
+nocolor='\033[0m'    # no color
+color6='\033[0;32m'  # Green
+color8='\033[0;38;5;208m' # Bold Orange
+printf "Welcome to ${color6}%s ${nocolor}\n" "$(uname -n)"
+#printf "${color8}%s ${nocolor}(%s %s %s)\n" "$DISTRIB_DESCRIPTION" "$(uname -o)" "$(uname -r)" "$(uname -m)"
+#printf "\n" "\n" "$DISTRIB_DESCRIPTION" "$(uname -n)" "$(uname -r)" "$(uname -m)" "\n"
+
+
+## Black        0;30     Dark Gray     1;30
+## Red          0;31     Light Red     1;31
+## Green        0;32     Light Green   1;32
+## Brown/Orange 0;33     Yellow        1;33
+## Blue         0;34     Light Blue    1;34
+## Purple       0;35     Light Purple  1;35
+## Cyan         0;36     Light Cyan    1;36
+## Light Gray   0;37     White         1;37
+
+color1='\033[1;31m'  # light red
+color2='\033[1;35m'  # light purple
+color3='\033[01;38;5;220m'  # Yellow
+color4='\033[1;33m'  # Brown/Orange
+color5='\033[1;37m'  # White
+color6='\033[0;32m'  # Green
+color7='\033[1;32m'  # Light Green
+color8='\033[01;38;5;208m' # Bold Orange
+color9='\033[1;34m'  # light blue
+nocolor='\033[0m'    # no color
+
+printf "               ${color1}.-.${nocolor}\n"
+printf "         ${color8}.-'\`\`${color1}(   ) ${nocolor}\n"
+printf "      ${color3},\`\\ ${color8}\\    ${color1}\`-\`${color8}.${nocolor}\n"
+printf "     ${color3}/   \\ ${color8}'\`\`-.   \`   ${color8}`lsb_release -s -d`${nocolor}\n"
+printf "   ${color8}.-.  ${color3},       ${color8}\`___:  ${color3}`uname -srmo`${nocolor}\n"
+printf "  ${color8}(   ) ${color3}:       ${color1} ___   ${color1}`date`${nocolor}\n"
+printf "   ${color8}\`-\`  ${color3}\`      ${color1} ,   :${nocolor}\n"
+printf "     ${color3}\\   / ${color1},..-\`   ,${nocolor}\n"
+printf "      ${color3}\`./${color1} /    ${color3}.-.${color1}\`${nocolor}\n"
+printf "         ${color1}\`-..-${color3}(   )${nocolor}\n"
+printf "               ${color3}\`-\`${nocolor}\n"
+
+printf "        .--. \n"
+printf "       |${color6}o${nocolor}_${color6}o${nocolor} | \n"
+printf "       |${color3}\_/${nocolor} | \n"
+printf "      //   \\ \\ \n"
+printf "     (|     | )	 \n"
+printf "    ${color3}/${nocolor}\'\_   _/\'${color3}\ \n"
+printf "    ${color3}\\___)${nocolor}=${color3}(___/ \n"
+printf "\n"
+printf "\n"
